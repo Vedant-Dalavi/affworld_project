def619887defbdd4695f749c9b671203b30e835b
Vedant-Dalavi <154957679+Vedant-Dalavi@users.noreply.github.com>
2025-01-26 17:30:47 +0530

    signup file

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,13 +1,9 @@
-import signupImg from "../assets/Images/login.png"
 import Template from "../components/core/Auth/Template"
 
 function Signup() {
     return (
         <Template
             title="Create Account"
-            description1="Build skills for today, tomorrow, and beyond."
-            description2="Education to future-proof your career."
-            image={signupImg}
             formType="signup"
         />
     )
